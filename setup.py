82202eda806a4c39306181db74b3493f7c5b4844
Rohit Anand <161624817+R0h1tAnand@users.noreply.github.com>
2025-09-17 13:56:41 +0000

    Update entry point for console script to use 'main' function in dw_search.core

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description="DW-Search is a script that scrapes urls on different .onion search engines.",
     include_package_data=True,
     url='https://github.com/R0h1tAnand/DW-Search',
-    entry_points = {'console_scripts': ['dw-search = dw_search.core:scrape']},
+    entry_points = {'console_scripts': ['dw-search = dw_search.core:main']},
     classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
